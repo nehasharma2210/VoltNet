8557425428c44d6b1736e01f9845591ccc99518b
Neha sharma <u24ai050@aid.svnit.ac.in>
2025-11-02 18:32:27 +0530

    Fix BACKEND_URL syntax in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
 // This file is generated at runtime from BACKEND_URL environment variable
-window.BACKEND_URL = '${https://volt-net-backend.onrender.com}';
+const BACKEND_URL = "${BACKEND_URL}";
+
+
 
